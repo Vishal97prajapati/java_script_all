b5cfb9ea1f1af5bd594ff235f5a8ea4ab4e0c5f3
Vishal Kumar Prajapati <130454128+Vishal97prajapati@users.noreply.github.com>
2024-05-06 16:40:52 +0000

    all data types

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,27 @@
+" use strict "; // treat all java script codes as new version 
+
+//https://tc39.es/ecma262/#sec-tokens imprtance link ecma scripts
+
+
+ // Data tyoes in java scripts 
+
+  // numer =. 2 to power 53 its number of range in java scripts 
+  // bigint
+  // string
+  // boolean => true/ false 
+  // null => standlone value 
+  // undefine = >
+  // symbol => used for unique 
+
+
+   // OBJECT  is java script
+
+
+ let name = "vishal"
+ let age  = 26
+ let isloggedin  =  false 
+
+console.log(typeof "name")
+
+console.log(typeof age )
+console.log( typeof isloggedin)
