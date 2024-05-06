be11e520c6b2651eb4f9c49a857df0ec12992321
Vishal Kumar Prajapati <130454128+Vishal97prajapati@users.noreply.github.com>
2024-05-06 17:03:13 +0000

    conversion of data types

---
+++ 01_basics/03_talk_operation.js
@@ -0,0 +1,41 @@
+//  let score = "33abcd"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+let score = undefined;
+
+console.log(typeof score);
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// how to covert a number in boolean
+
+let islogged = 1;
+
+let boolisLoggedIn = Boolean(islogged);
+console.log(boolisLoggedIn);
+
+// 1 = > trur ; 0 => false
+// "" => false
+// "vishal" => true
+
+//how to convert a in string
+
+let somenumber = 45;
+
+let stringnumber = String(somenumber);
+console.log(typeof stringnumber);
